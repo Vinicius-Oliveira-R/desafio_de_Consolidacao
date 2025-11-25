d96261166cb16876d0adb8da01e3cae7ca0ea221
Vinicius-Oliveira-R <viniciusoliveira12353@gmail.com>
2025-11-25 20:45:00 -0300

    exemplo correto sobre como controlar o espaço de Column e Row

--- lib/simulado_widgets/questoes/questao004.dart
+++ lib/simulado_widgets/questoes/questao004.dart
@@ -1,49 +1,67 @@
 // //Estrutura e Layout
 // //Quando o widget "X" é posicionado dentro de widgets como Column ou Row, quais cuidados de layout e alinhamento devem ser observados?
 // //Explique e demonstre com código um cenário de erro e outro corrigido.
-// Os cuidados que devemos ter com o posicionamento de Column e Row dentro de widgets, é sua expansão infinita, se nós não definirmos um limite utilizando EXPANDED alguns widgets como ListView atribuem um tamanha infinito  
+// Os cuidados que devemos ter com o posicionamento de Column e Row dentro de widgets, é sua expansão infinita, se nós não definirmos um limite utilizando EXPANDED alguns widgets como ListView atribuem um tamanha infinito
+
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBaar(title: cont Text("maneira errada")),
+//         body: Column(
+//           children:[
+//               ListView(
+//                 children: const[
+//                   ListTitle(title: Text("Lista 1")),
+//                   ListTitle(title: Text("Lista 2")),
+//                   ListTitle(title: Text("Lista 3")),
+//                   ListTitle(title: Text("Lista 4")),
+//                 ],
+//               ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp( // Root widget
-  //     home: Scaffold(
-  //       appBar: AppBar(
-  //         title: const Text('My Home Page'),
-  //       ),
-  //       body: Center(
-  //         child: Builder(
-  //           builder: (context) {
-  //             return Column(
-  //               children: [
-  //                 const Text('Hello, World!'),
-  //                 const SizedBox(height: 50),
-  //                 ElevatedButton(
-  //                   onPressed: () {
-  //                     print('Click!');
-  //                   },
-  //                   child: const Text('A button'),
-  //                 ),
-  //               ],
-  //             );
-  //           },
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: const Text('ola'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Maneira correta de impor limites de espaço ")),
+          body:Row( 
+            children: [ 
+              Column(
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        children: 
+                          const[
+                            ListTile(title: Text("item 1")),
+                            ListTile(title: Text("item 2")),
+                          ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+          ),
+        ),
     );
   }
-} 
+}
